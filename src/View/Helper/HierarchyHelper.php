e92e4b690210fe7afd8a577f69bfd9c039b9b5eb
David Yell <dyell@ukwebmedia.com>
2015-07-29 15:20:31 +0100

    Remove the extra ul

--- src/View/Helper/HierarchyHelper.php
+++ src/View/Helper/HierarchyHelper.php
@@ -19,15 +19,16 @@ class HierarchyHelper extends Helper
     public function tree($thread, &$out = null)
     {
         if (!empty($thread) && is_array($thread)) {
+            $out .= "<ul>";
             foreach ($thread as $item) {
-                $out .= "<ul>";
-                $out .= "<li><input type='number' maxlength='3' size='3' min='0' max='{$item->quantity}'>/" . $item->quantity . ' ' . $item->ingredient->name;
+
+                $out .= "<li><span><input type='number' maxlength='3' size='3' min='0' max='{$item->quantity}'>/" . $item->quantity . ' ' . $item->ingredient->name . '</span>';
                 if (!empty($item->children)) {
                     $this->tree($item->children, $out);
                 }
                 $out .= "</li>";
-                $out .= "</ul>";
             }
+            $out .= "</ul>";
         }
 
         return $out;
